4c6a8c8dc8945ed23787cb3b0636d3f569691e21
k-cormack <k.cormack@gmail.com>
2018-10-17 13:40:28 -0600

    Minor dropdown fix, beforeDestroy( ) listener

--- wwwsrc/src/components/AllKeeps.vue
+++ wwwsrc/src/components/AllKeeps.vue
@@ -34,12 +34,7 @@
         name: "all-keeps",
         data() {
             return {
-                // newKeep: {
-                //     name: "",
-                //     description: "",
-                //     img: "",
-                //     vaultId: this.$route.params.vaultId
-                // }
+
             }
         },
         mounted() {
@@ -48,13 +43,6 @@
                 this.$store.dispatch('getKeeps');
             }
             window.addEventListener('click', this.closeDropdown);
-            // if (this.$store.state.myVaults.length == 0 && this.$store.state.user.id) {
-            //     let userId = this.$store.state.user.id;
-            //     this.$store.dispatch('getMyVaultsForAdd', userId)
-            // }
-
-            // this.$store.dispatch('getVault', this.$route.params.vaultId);
-            // this.$store.dispatch('getVaultKeeps', this.$route.params.vaultId)
         },
         computed: {
 
@@ -70,38 +58,30 @@
         },
         methods: {
             closeDropdown() {
-
                 let id1 = this.$store.state.dropdownId1;
                 let id2 = this.$store.state.dropdownId2;
-                if (id1 == 0) {
+                if (id1 == 0 && id1 != id2) {                
                     let x = document.getElementById(id2);
                     x.style.display = "none";
                     this.$store.state.dropdownId1 = 0;
-
-                    // document.getElementById(id2).classList.toggle("show");
-                    // this.$store.state.dropdownId = {}
                 } else if (id2 != 0) {
                     let x = document.getElementById(id2);
                     x.style.display = "none";
                     if (id2 == id1) {
                         this.$store.state.dropdownId2 = 0;
                     } else {
-
                         this.$store.state.dropdownId2 = id1;
                     }
                     return
                 }
                 this.$store.state.dropdownId2 = this.$store.state.dropdownId1;
                 this.$store.state.dropdownId1 = 0;
-
             },
-            //             var x = document.getElementById(this.keepData.id);
-            //     if (id1.style.display === "block") {
-            //         x.style.display = "none";
-            //     } else {
-            //         x.style.display = "none";
-            //     }
-            // }
+        },
+        beforeDestroy(){
+            window.removeEventListener('click', this.closeDropdown);
+            this.$store.state.dropdownId1 = 0;
+            this.$store.state.dropdownId2 = 0;
         },
 
         components: {
--- wwwsrc/src/components/Vault.vue
+++ wwwsrc/src/components/Vault.vue
@@ -1,26 +1,13 @@
 <template>
     <div class="vault">
-        <!-- <Navbar></Navbar> -->
+    
         <CreateKeepBar></CreateKeepBar>
-        <!-- <div id="keeps">
-            <h3>{{vault.name}}</h3>
-            <h5>{{vault.description}}</h5>
-            <hr>
-            <form @submit.prevent="addKeep">
-                <input class="newKeep1" type="text" placeholder="New Keep Name" v-model="newKeep.name" required>
-                <input class="newKeep2" type="text" placeholder="Description" v-model="newKeep.description">
-                <input class="newKeep3" type="text" placeholder="Img Url" v-model="newKeep.img">
-                <button class="submit" type="submit">Create New Keep</button>
-            </form>
-        </div> -->
 
         <div id="keeps" class="row justify-content-center">
-            
-            <Keep v-for="keep in vaultKeeps" :keepData='keep' :key="keep.id" />
-            
-        </div>
-        <!-- <span>{{vaultData.name}}</span> -->
 
+            <Keep v-for="keep in vaultKeeps" :keepData='keep' :key="keep.id" />
+
+        </div>
 
     </div>
 </template>
@@ -34,17 +21,12 @@
         name: "vault",
         data() {
             return {
-                // newKeep: {
-                //     name: "",
-                //     description: "",
-                //     img: "",
-                //     vaultId: this.$route.params.vaultId
-                // }
+               
             }
         },
         mounted() {
-            this.$store.dispatch("authenticateInVault");
-
+            this.$store.dispatch("authenticateForVault");
+            window.addEventListener('click', this.closeDropdown);            
             this.$store.dispatch('getVault', this.$route.params.vaultId);
             this.$store.dispatch('getVaultKeeps', this.$route.params.vaultId)
         },
@@ -59,9 +41,34 @@
         methods: {
             addKeep() {
                 this.$store.dispatch('addKeep', this.newKeep);
-                // this.$store.dispatch('addKeeptoVault', this.$route.params.vaultId)
-            }
+            },
+            closeDropdown() {
+                let id1 = this.$store.state.dropdownId1;
+                let id2 = this.$store.state.dropdownId2;
+                if (id1 == 0 && id1 != id2) {
+                    let x = document.getElementById(id2);
+                    x.style.display = "none";
+                    this.$store.state.dropdownId1 = 0;
+                } else if (id2 != 0) {
+                    let x = document.getElementById(id2);
+                    x.style.display = "none";
+                    if (id2 == id1) {
+                        this.$store.state.dropdownId2 = 0;
+                    } else {
+                        this.$store.state.dropdownId2 = id1;
+                    }
+                    return
+                }
+                this.$store.state.dropdownId2 = this.$store.state.dropdownId1;
+                this.$store.state.dropdownId1 = 0;
+            },
         },
+        beforeDestroy(){
+            window.removeEventListener('click', this.closeDropdown);
+            this.$store.state.dropdownId1 = 0;
+            this.$store.state.dropdownId2 = 0;
+        },
+        
         props: ["vaultData"],
 
         components: {
@@ -73,12 +80,7 @@
 </script>
 
 <style scoped>
-    /* .newKeep1,
-    .newKeep2 {
-        margin-right: 5px
-    } */
     #keeps {
         margin-top: 120px;
     }
-   
 </style>
